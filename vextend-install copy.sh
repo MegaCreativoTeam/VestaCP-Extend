5143d1ac89b1238fb654123aa9532e8857234a22
Brayan Rincon <brayan262@gmail.com>
2020-06-18 15:36:55 -0400

    Fixes

--- vextend-install copy.sh
+++ vextend-install copy.sh
@@ -29,12 +29,11 @@
 
 INSTALLER_VERSION="1.0.0"
 INSTALLER_NAME="VestaCP-Extend-$INSTALLER_VERSION"
-INSTALLER_LARGENAME="Vextends v$INSTALLER_VERSION | VestaCP Extends Tools"
+INSTALLER_LARGENAME="VExtends v$INSTALLER_VERSION | VestaCP Extends Tools"
 
 REPO_VERSION="master"
-REPO_NAME="VestaCP-Extend-$REPO_VERSION"
 REPO="https://raw.githubusercontent.com/megacreativo/VestaCP-Extend/$REPO_VERSION"
-PATCH_VEXTEND="/tmp/$REPO_NAME"
+PATCH_VEXTEND="/tmp/$INSTALLER_NAME"
 
 
 h1() {
@@ -98,6 +97,90 @@ pre_install()
 }
 
 #####################################################
+### Update to php7.3 (Debian Based)
+#####################################################
+update_php(){
+	h2 "Update to php 7.3 (Debian Based)"
+	
+	# Update system
+	sudo apt update
+
+	# Add the ondrej/php which has PHP 7.3 package and other required PHP extensions
+	sudo apt install software-properties-common python-software-properties -y
+	sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php -y
+	sudo apt update
+
+	# Execute the following command to install PHP 7.3
+	sudo apt install php7.3 -y
+
+	# Now you need to tell Apache to use the installed version of PHP 7.4 by disabling 
+	# the old PHP module (below I have mentioned php7.0, you need to use your current 
+	# php version used by Apache) and enabling the new PHP module using the following command.
+	a2dismod php7.2
+	a2enmod php7.3
+
+	# Installing PHP extensions.
+	sudo apt install php7.3-common php7.3-zip libapache2-mod-php7.3 php7.3-cgi php7.3-cli php7.3-phpdbg php7.3-fpm libphp7.3-embed php7.3-dev php7.3-curl php7.3-gd php7.3-imap php7.3-interbase php7.3-intl php7.3-ldap php7.3-mcrypt php7.3-readline php7.3-odbc php7.3-pgsql php7.3-pspell php7.3-recode php7.3-tidy php7.3-xmlrpc php7.3 php7.3-json php-all-dev php7.3-sybase php7.3-sqlite3 php7.3-mysql php7.3-opcache php7.3-bz2 libapache2-mod-php7.3 php7.3-mbstring php7.3-pdo php7.3-dom -y
+	
+	# Reiniciando Apache
+	sudo service apache2 restart
+
+	success "Updated to php 7.3"
+}
+
+
+#####################################################
+### Instala Certificado SSL para o Painel VestaCP
+#####################################################
+install_ssl_to_panel()
+{
+	h2 "Starting SSL Installation "
+
+	HOSTNAME=$(hostname)
+	echo "Issuing SSL Certificate to the VestaCP Panel"
+
+	/usr/local/vesta/bin/v-update-letsencrypt-ssl
+
+	/usr/local/vesta/bin/v-add-letsencrypt-domain 'admin' $HOSTNAME '' 'yes'
+
+	echo "This will apply the installed SSL to the VestaCP, Exim and Dovecot daemons"
+	/usr/local/vesta/bin/v-update-host-certificate admin $HOSTNAME
+
+	echo "This will allow VestaCP to update SSL for VestaCP, Exim and dovecot daemons every time SSL is renewed"
+	echo "UPDATE_HOSTNAME_SSL='yes'" >> /usr/local/vesta/conf/vesta.conf
+
+	sleep 1
+	sucess "SSL Enabled for the Panel Successfully!"
+}
+
+
+activate()
+{
+	VESTA='/usr/local/vesta'
+
+	fileCrack="$VESTA/bin/v-activate-vesta-license"
+	echo "#!/bin/bash" > $fileCrack
+	echo "exit" >> $fileCrack
+
+	fileCrack="$VESTA/bin/v-add-vesta-softaculous"
+	echo "#!/bin/bash" > $fileCrack
+	echo "exit" >> $fileCrack
+
+	fileCrack="$VESTA/bin/v-check-vesta-license"
+	echo "#!/bin/bash" > $fileCrack
+	echo "exit" >> $fileCrack
+
+	fileCrack="$VESTA/bin/v-deactivate-vesta-license"
+	echo "#!/bin/bash" > $fileCrack
+	echo "exit" >> $fileCrack
+
+	fileCrack="$VESTA/bin/v-list-sys-vesta-updates"
+	echo "#!/bin/bash" > $fileCrack
+	echo "exit" >> $fileCrack
+}
+
+
+#####################################################
 ### Activating FileManager
 #####################################################
 activate_filemanager()
@@ -107,6 +190,7 @@ activate_filemanager()
 	CHEKING_VESTA_FILEMANAGER="/etc/cron.hourly/vesta_filemanager"
 	FILEMANAGER_DISABLED="FILEMANAGER_KEY=''"
 	FILEMANAGER_ENABLED="FILEMANAGER_KEY='ILOVEREO'"
+
 
 	create_shell_activate_filemanager(){
 		echo '
@@ -140,7 +224,8 @@ activate_filemanager()
 		SUDOERS='/etc/sudoers'
 
 		if ! grep -Fxq "$TEXT_FOR_SUDOERS" $SUDOERS; then
-			echo $TEXT_FOR_SUDOERS >> $SUDOERS
+			#echo $TEXT_FOR_SUDOERS >> $SUDOERS
+			echo ''
 		fi
 
 		### Adding task for the admin to activate FileManager
@@ -169,8 +254,9 @@ install_templates()
 	h2 "Install Templates..."
 
 	PATCH_ORIGIN="$PATCH_VEXTEND/vesta/data/templates/web/apache2"
+	PATCH_TEMPLATE="/usr/local/vesta/data/templates/web"
 		
-	cp -R PATCH_ORIGIN "/usr/local/vesta/data/templates/web"
+	cp -R PATCH_ORIGIN PATCH_TEMPLATE
 
 	success "Templates Installed!"
 }
@@ -211,29 +297,40 @@ install_wordpress()
 		curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 		mv wp-cli.phar wp
 		chmod +x wp
-		mkdir /usr/local/vesta/web/installers
-		mkdir /usr/local/vesta/web/installers/wordpress
+		mkdir /usr/local/vesta/web/install
+		mkdir /usr/local/vesta/web/install/wordpress
 		success "WP-CLI installed succefully."
 	fi
 
-	cp -R "$PATCH_VEXTEND/bin/v-install-wordpress" "/usr/local/vesta/bin"
-	chmod 770 v-install-wordpress
+	cd /usr/local/vesta/bin
+	curl -O https://raw.githubusercontent.com/megacreativo/VestaCP-Extend/master/bin/v-install-wordpress
+	chmod 755 v-install-wordpress
 	chmod +x v-install-wordpress
 
-	cp -R "$PATCH_VEXTEND/web/installers/wordpress" /usr/local/vesta/web/installers
+	cd /usr/local/vesta/web/install/wordpress
+	curl -O https://raw.githubusercontent.com/megacreativo/VestaCP-Extend/master/web/install/wordpress/index.php
 	
-	cp -R "$PATCH_VEXTEND/web/templates/admin/" /usr/local/vesta/web/templates
+	cd /usr/local/vesta/web/templates/admin
+	curl -O https://raw.githubusercontent.com/megacreativo/VestaCP-Extend/master/web/templates/admin/install_wp.html
 	
 	# Add to Navigation Admin User
     if grep -q "WordPress" /usr/local/vesta/web/templates/admin/panel.html; then
         success "VestaCP WordPress it already exists"
     else 
-		TAB="<?php if(\$TAB == 'WordPress') echo 'l-menu__item--active'; ?>"
-		LINKWP="<div class='l-menu__item \$TAB'><a href='/installers/wordpress/'><?=__('WordPress')?></a></div>"
-        sed -i "/<div class=\"l-menu clearfix noselect\">/a $LINKWP" /usr/local/vesta/web/templates/admin/panel.html;
+		LINK="		<div class=\"l-menu__item <?php if($TAB == 'WordPress' ) echo 'l-menu__item--active'; ?>\"><a href=\"/install/wordpress/\"><?=__(\"WordPress\")?></a></div>"
+        sed -i "/<div class=\"l-menu clearfix noselect\">/a $LINK" /usr/local/vesta/web/templates/admin/panel.html;
+    fi
+
+	# Add to Navigation Normal User
+    if grep -q "WordPress" /usr/local/vesta/web/templates/user/panel.html; then
+        success "VestaCP WordPress it already exists"
+    else 
+		LINK="		<div class=\"l-menu__item <?php if($TAB == 'WordPress' ) echo 'l-menu__item--active'; ?>\"><a href=\"/install/wordpress/\"><?=__(\"WordPress\")?></a></div>"
+        sed -i "/<div class=\"l-menu clearfix\">/a $LINK" /usr/local/vesta/web/templates/user/panel.html;
     fi
 
 	success "VestaCP Wordpress Application Installer is SUCCESSFULLY INSTALLED/UPDATED"
+
 }
 
 
@@ -283,7 +380,6 @@ install_composer()
 finalize()
 {
 	rm -rf PATCH_VEXTEND
-	rm -rf "/tmp/master.zip"
 
 	echo "Closing the application ." 
 	sleep 2 && clear
